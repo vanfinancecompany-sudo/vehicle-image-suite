ee55b644fb1a725ae713b22361966a43f3aeb0be
vanfinancecompany-sudo <vanfinancecompany@gmail.com>
2026-04-19 00:24:35 +0100

    Update image.js

--- api/image.js
+++ api/image.js
@@ -1,1 +1,17 @@
+export default async function handler(req, res) {
+  try {
+    const { url } = req.query;
 
+    if (!url) {
+      return res.status(400).send("Missing URL");
+    }
+
+    const response = await fetch(url);
+    const buffer = await response.arrayBuffer();
+
+    res.setHeader("Content-Type", "image/jpeg");
+    res.send(Buffer.from(buffer));
+  } catch (err) {
+    res.status(500).send("Image proxy failed");
+  }
+}
